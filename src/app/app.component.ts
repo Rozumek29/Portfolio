614f19c7dda9a9dd931359a5c831c0f878da94a3
Rozumek29 <k.wyrozumialski@a-soft.pl>
2023-04-19 22:19:42 +0200

    Scroll handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  Component, Directive, ElementRef, HostListener, Inject,
+  OnInit, ViewChild,
+  ViewChildren
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit {
+
+  @ViewChildren('section') sections: ElementRef[];
+  @ViewChild('main') main: ElementRef;
+  @ViewChild('navigation') navigation: ElementRef;
+
+  loader: boolean = false;
+  windowHeight: number = window.innerHeight;
+
+  constructor() {
+
+  }
+
+  ngOnInit() : void {
+    this.loader = false;
+    setTimeout(() => this.loader = false, 3000)
+  }
+
+  @HostListener('mousewheel', [])
+  onScroll() : void {
+    const scrollTop = this.main.nativeElement.scrollTop;
+    this.sections.forEach((section: ElementRef, index: number) : void => {
+      if ((section.nativeElement.offsetTop < this.main.nativeElement.scrollTop + this.windowHeight / 2) && (scrollTop < section.nativeElement.offsetTop + this.windowHeight / 2)){
+        this.resetNavigation();
+        this.navigation.nativeElement.children[index].classList.add('selected');
+      }
+    })
+  }
+
+  resetNavigation() : void {
+    for (let i: number = 0; i < this.navigation.nativeElement.children.length; i++) {
+      this.navigation.nativeElement.children[i].classList.remove('selected');
+    }
+  }
+
+  scrollToSection(index: number) : void {
+    this.resetNavigation();
+    const section = this.sections.find((e, i) => i == index );
+    section?.nativeElement.scrollIntoView({
+      behavior: 'smooth',
+    })
+    this.navigation.nativeElement.children[index].classList.add('selected');
+  }
 }
